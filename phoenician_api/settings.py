48e5ab4cc4f19f40e1a0554fbf627e835b1324f9
James Joshua Ginete <joshua@ingenuity.ph>
2020-07-27 19:37:45 +0800

    Minimal fixes

--- phoenician_api/settings.py
+++ phoenician_api/settings.py
@@ -91,12 +91,13 @@ WSGI_APPLICATION = 'phoenician_api.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'].update(db_from_env)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
