f6f2b1d51502619d8b6e950eb3e65c79ae386e73
James Joshua Ginete <joshua@ingenuity.ph>
2020-07-29 15:24:32 +0800

    Fix the response body for signup endpoint

--- users/views.py
+++ users/views.py
@@ -35,7 +35,7 @@ class UserSignupView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
