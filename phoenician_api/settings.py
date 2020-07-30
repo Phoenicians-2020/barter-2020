3c05f20cc7ff5aa23f1dca4c796aed9282ddd4f1
James Joshua Ginete <joshua@ingenuity.ph>
2020-07-30 14:25:43 +0800

    Fixes

--- phoenician_api/settings.py
+++ phoenician_api/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*=kog!z^*oqp02f3vrt-o94j$qoj#c8i*k^pe6no#d_mm9hib&'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = '*=kog!z^*oqp02f3vrt-o94j$qoj#c8i*k^pe6no#d_mm9hib&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+# DEBUG = True
 
 ALLOWED_HOSTS = ['phoenician-api.herokuapp.com']
 
